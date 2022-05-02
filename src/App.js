36b3fa83fcde2707a33fa8ed37996ab4ba7812a5
Ruben <ruben.iannelli@hotmail.com>
2022-05-02 20:13:03 +0200

    under constructions

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <title>OVERVERSE LABS | Blockchain</title>
+        <link rel="icon" href="/favicon.ico" />
+      </header>
+
+      <main
+        style={{
+          textAlign: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography
+          style={{
+            paddingTop: "30vh",
+            fontSize: "10vw",
+            fontFamily: "Titillium Web",
+          }}
+          variant="h1"
+        >
+          OverVerse Labs
+        </Typography>
+        <Typography
+          style={{ fontSize: "3vw", fontFamily: "Titillium Web" }}
+          variant="h5"
+        >
+          🚧 WEBSITE UNDER CONSTRUCTION 🚧
+        </Typography>
+      </main>
+    </div>
+  );
+}
+
+export default App;
